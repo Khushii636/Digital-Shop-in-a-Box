7c2121b7fde7c75c67df12a6335b172121fa2bd5
Khushii636 <khushichowdhary18@gmail.com>
2026-08-27 22:56:07 +0530

    feat(auth): setup accounts app and custom User model

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'apps.accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
